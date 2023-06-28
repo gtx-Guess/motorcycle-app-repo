2f742d3935d95a60df20aa5a0726009b0db39331
Tigran <tigran@Tigrans-MacBook-Air.local>
2023-06-27 20:38:22 -0700

    Deleting the branch comments

--- react-frontend/src/components/MotoCards.jsx
+++ react-frontend/src/components/MotoCards.jsx
@@ -1,9 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import EditMoto from "./EditMoto";
 
-//making dev branch
-//making branch from dev branch
-//making branch3 to trigger deployment to dev enviornment in railway
+//deleting comments
 const MotoCards = () => {
 
     return (
