c7326dbf9025ab3df659bbd83bc5890d1602eeca
Tigran <tigran@Tigrans-MacBook-Air.local>
2023-06-27 20:17:37 -0700

    adding last nights stuff

--- react-frontend/src/components/Container.jsx
+++ react-frontend/src/components/Container.jsx
@@ -1,55 +1,59 @@
-import React, { useRef, useState, useEffect } from 'react';
-import axios from 'axios';
-import Motorcycles from './Motorcycles';
-import FilterBar from './FilterBar';
-import MotoForm from './MotoForm';
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import Motorcycles from "./Motorcycles";
+import FilterBar from "./FilterBar";
+import MotoForm from "./MotoForm";
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const Container = () => {
-  const containerRef = useRef(null);
-  const contBottom = useRef(null);
-  const [motoList, motoSetter] = useState([]);
+    const containerRef = useRef(null);
+    const contBottom = useRef(null);
+    const [motoList, motoSetter] = useState([]);
 
-  const getMotoData = async () => {
-    console.log('getMotoData');
-    const response = await axios.get(`${BASE_URL}/getMotos`);
-    let resp = [...response.data];
-    resp = resp.sort( (a,b) => ( a["brand"].localeCompare(b["brand"]) ) );
-    motoSetter([...resp]);
-  };
+    const getMotoData = async () => {
+        console.log("getMotoData");
+        const response = await axios.get(`${BASE_URL}/getMotos`);
+        let resp = [...response.data];
+        resp = resp.sort((a, b) => a["brand"].localeCompare(b["brand"]));
+        motoSetter([...resp]);
+    };
 
-  useEffect(() => {
-    console.log('Getting all moto data from supabase with getMotoData');
-    getMotoData();
-  }, []);
+    useEffect(() => {
+        console.log("Getting all moto data from supabase with getMotoData");
+        getMotoData();
+    }, []);
 
-  useEffect(() => {
-    console.log('Motolist state has been updated, re-rending list');
-  }, [motoList]);
+    useEffect(() => {
+        console.log("Motolist state has been updated, re-rending list");
+    }, [motoList]);
 
-  const toggleModal = () => {
-      const form = document.getElementById('moto-form-modal-div');
-      const blur = document.getElementById('background-blur');
-      blur.classList.toggle('hide-moto-modal');
-      form.classList.toggle('hide-moto-modal');
-  };
+    const toggleModal = () => {
+        const form = document.getElementById("moto-form-modal-div");
+        const blur = document.getElementById("background-blur");
+        blur.classList.toggle("hide-moto-modal");
+        form.classList.toggle("hide-moto-modal");
+    };
 
-  return (
-    <div id="main-container" className={'container'} ref={containerRef}>      
-      <FilterBar motoList={motoList}/>
-      <MotoForm props={[toggleModal, getMotoData]}/>
-      <div id='container-bottom' className={'container-bottom'} ref={contBottom}>
-        <ul id="left-list" className={'left_ul'} >
-          <br/>
-          <Motorcycles props={[motoList, contBottom]}/>
-          <span style={{ position: 'sticky', top: '86%' }}>
-            <li className={'add-moto-li'} onClick={toggleModal}>Add Moto</li>
-            <li className={'add-moto-li'}>Delete Moto</li>
-          </span>
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div id="main-container" className={"container"} ref={containerRef}>
+            <FilterBar motoList={motoList} />
+            <MotoForm props={[toggleModal, getMotoData]} />
+            <div
+                id="container-bottom"
+                className={"container-bottom"}
+                ref={contBottom}
+            >
+                <ul id="left-list" className={"left_ul"}>
+                    <br />
+                    <Motorcycles props={[motoList, contBottom]} />
+                    <div className={"add-delete-buttons"}>
+                        <li className={"add-moto-li"} onClick={toggleModal}>Add Moto</li>
+                        <li className={"add-moto-li"}>Delete Moto</li>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Container;
