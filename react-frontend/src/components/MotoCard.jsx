86d817319fd55653387e79a19670a8f97b7041f0
Tigran <tigran@Tigrans-MacBook-Air.local>
2023-07-08 23:02:28 -0700

    Made ui updates and optimized stateful variables

--- react-frontend/src/components/MotoCard.jsx
+++ react-frontend/src/components/MotoCard.jsx
@@ -1,12 +1,17 @@
+import axios from 'axios';
 import { useState } from 'react';
 import EditMoto from "./EditMoto";
-import axios from 'axios';
 import ErrorBubble from "./ErrorBubble";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 const BASE_URL = import.meta.env.VITE_SERVER_URL;
 
 const MotoCard = ({ props }) => {
     const [showError, setErrorState] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
+    const [showClose, setShowClose] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
+
     const moto = props[0];
     const index = props[1];
     const getMotoData = props[2];
@@ -15,8 +20,16 @@ const MotoCard = ({ props }) => {
     const motoImageLink = S3_BASE_URL + moto.imageLink;
     const overLay = document.getElementById("overlay");
 
-    const handleUpdateMotoBtnClick = async (event) => {
-        let reset = true;
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+  
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+    const resetMotoHtmlValues = (closeBtn=false) => {
         const brandSpan = document.getElementById(`${moto.id}-brand-value-span`);
         const updatedBrand = !brandSpan.querySelector("input").value ? moto.brand  : brandSpan.querySelector("input").value;
         brandSpan.innerHTML = updatedBrand;
@@ -32,6 +45,27 @@ const MotoCard = ({ props }) => {
         const ccSpan = document.getElementById(`${moto.id}-cc-value-span`);
         const updatedCC = !ccSpan.querySelector("input").value ? moto.cc : ccSpan.querySelector("input").value;
         ccSpan.innerHTML = updatedCC;
+
+        if(closeBtn){
+            setShowClose(false);
+            const card = document.getElementById(`${moto.name}_${moto.id}_card`);
+            card.classList.toggle('to-front');
+    
+            const gear = document.getElementById(`${moto.id}-update-gear`);
+            gear.classList.toggle('hide-edit-btns');
+    
+            const updateButtonsDiv = card.querySelector('#edit-buttons-div');
+            updateButtonsDiv.classList.toggle('hide-edit-btns');
+            overLay.classList.toggle("hide");
+            return;
+        };
+        
+        return [updatedBrand, updatedName, updatedYear, updatedCC];
+    };
+
+    const handleUpdateMotoBtnClick = async (event) => {
+        let reset = true;
+        const [updatedBrand, updatedName, updatedYear, updatedCC] = resetMotoHtmlValues();
 
         const updatedMotoData = {
             "id": moto.id,
@@ -63,7 +97,7 @@ const MotoCard = ({ props }) => {
         card.classList.toggle('to-front');
 
         const gear = document.getElementById(`${moto.id}-update-gear`);
-        gear.classList.toggle('block-click');
+        gear.classList.toggle('hide-edit-btns');
 
         const updateButtonsDiv = card.querySelector('#edit-buttons-div');
         updateButtonsDiv.classList.toggle('hide-edit-btns');
@@ -95,12 +129,13 @@ const MotoCard = ({ props }) => {
         <div className={index === 0 ? "card" : "card hide-card"} id={`${moto.name}_${moto.id}_card`}>
             <div className={"edit-top-div"}>
                 <p className={'edit-moto-brand-p-tag'}><span className={"cardSpan"}>Brand:</span> <span id={`${moto.id}-brand-value-span`}>{moto.brand}</span></p>
-                <EditMoto moto={moto}/>
+                <EditMoto props={[moto, setShowClose]}/>
+                { showClose &&  <FontAwesomeIcon id={"fa-x-btn"} className={"x-btn"} icon={faX} onClick={() => resetMotoHtmlValues(showClose)}/>}
             </div>
             <p><span className={"cardSpan"}>Name:</span> <span id={`${moto.id}-name-value-span`}>{moto.name}</span></p>
             <p><span className={"cardSpan"}>Year:</span> <span id={`${moto.id}-year-value-span`}>{moto.year}</span></p>
             <p><span className={"cardSpan"}>Engine:</span> <span id={`${moto.id}-cc-value-span`}>{moto.cc}</span> CC"s</p>
-            <img src={motoImageLink} alt={moto.name} />
+            <img src={motoImageLink} alt={moto.name}/>
             <div id={'edit-buttons-div'} className={"hide-edit-btns edit-buttons-div"}>
                 <button className={"update-moto-btn"} onClick={handleUpdateMotoBtnClick}>Update</button>
                 <button className={"delete-moto-btn"} onClick={handleDeleteMotoBtn}>Delete Moto</button>
